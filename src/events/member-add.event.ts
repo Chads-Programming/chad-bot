dfd8d76544b28ee9aeb4e1c572e2ffec678b802a
Krb0 <maurodavidcaceres@gmail.com>
2024-05-29 18:25:55 -0300

    feature: member add event

---
+++ src/events/member-add.event.ts
@@ -0,0 +1,14 @@
+import type { GuildMember } from "discord.js";
+import { envs } from '@/config/env';
+
+
+export async function memberAddEvent(member: GuildMember) {
+    try {
+        if(member.user.bot) return
+        const defaultRole = envs.DEFAULT_ROLE_ID
+        if (!defaultRole) return
+        await member.roles.add(defaultRole);
+    } catch (error) {
+        console.error('[EVENTS - MEMBER-ADD] Error adding default role: ', error);
+    }
+}
