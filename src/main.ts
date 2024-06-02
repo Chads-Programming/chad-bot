871d74ae129b2ece8661c4f433eae6609d2bd2cd
Krb <87597855+Krb0@users.noreply.github.com>
2024-06-02 08:54:43 -0300

    feat(commands): added new member-count command (#3)
    * feat(commands): added new member-count command
    
    * chore: linting fixes

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import client from './client';
 import foxCommand from './commands/fox';
+import memberCountCommand from './commands/member-count';
 import userCommand from './commands/user';
 
 import { envs } from './config/env';
@@ -13,6 +14,7 @@ function bootstrap() {
 
   discordClient.registerSlashCommand(userCommand);
   discordClient.registerSlashCommand(foxCommand);
+  discordClient.registerSlashCommand(memberCountCommand);
 
   discordClient.listenInteractions();
   discordClient.deployCommands();
